77c8383cb5ce7dcb8c6848ebfd72a2a530f91786
SHEMA IRADUKUNDA Daniel <danieliradukunda00@gmail.com>
2025-07-13 19:36:15 +0200

    correcting calcutations in finance_calculator file

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -4,5 +4,5 @@ monthlyExpenses = int(input("Enter your monthly expenses: "))
 monthlySavings = monthlyIncome - monthlyExpenses
 
 projectedSavings = ((monthlySavings * 12) + (monthlySavings * 12 * 0.05))
-print(f"Your monthly savings is: ${monthlySavings}")
-print(f"Projected savings after one year, with interest, is: {int(projectedSavings)}")
+print(f"Your monthly savings is: ${int(monthlySavings)}.")
+print(f"Projected savings after one year, with interest, is: {int(projectedSavings)}.")
