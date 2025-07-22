4951903aaf1f4e7d0fefa25a07dfb67dc4e636c3
SHEMA IRADUKUNDA Daniel <danieliradukunda00@gmail.com>
2025-07-22 14:04:01 +0200

    match case cal correction

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -5,14 +5,18 @@ operation = input("Choose the operation (+, -, *, /): ")
 
 match operation:
   case "+":
-    print(f"The result of {num1} + {num2} is {num1 + num2}")
+    result = num1 + num2
+    print(f"The result is {result}")
   case "-":
-    print(f"The result of {num1} - {num2} is {num1 - num2}")
+    result = num1 - num2
+    print(f"The result is {result}")
   case "*":
-    print(f"The result of {num1} * {num2} is {num1 * num2}")
+    result = num1 * num2
+    print(f"The result is {result}")
   case "/":
+    result = num1 / num2
     if num2 != 0:
-      print(f"The result of {num1} / {num2} is {num1 / num2}")
+      print(f"The result is {result}")
     else:
       print("cannot divide by zero.")
   case _:
